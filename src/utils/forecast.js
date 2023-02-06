cc9888e43c39142616fd5ba1596df0a18d5707aa
Andrew Tomlinson <tomlia@uk.ibm.com>
2023-02-06 11:58:44 +0000

    adding new infor to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude,longitude,callback) => {
             callback("Unable to find location")
         }  
         else {
-            callback(undefined,body.current.weather_descriptions[0] +". It is currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degrees out.")
+            callback(undefined,body.current.weather_descriptions[0] +". It is currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degrees out. Wind speed is " + body.current.wind_speed + " from " + body.current.wind_dir+ ".")
              }
         })
 }
